b8b25cc0435008585420443887543d5e63a3330b
charleslong13 <charleslong13@icloud.com>
2021-12-13 12:57:11 -0600

    csl-deletePosts

--- src/components/feed/PostList.js
+++ src/components/feed/PostList.js
@@ -3,18 +3,33 @@ import { useHistory } from "react-router-dom"
 
 export const PostList = () => {
     const [posts, updatePosts] = useState([])
-
+    
     const history = useHistory()
 
     useEffect(
         () => {
-            fetch("http://localhost:8088/posts?_expand=user")
+            fetch(`http://localhost:8088/posts?_expand=user`)
             .then(res => res.json())
             .then((data) => {
                 updatePosts(data)
             })
         }, []
     )
+const deletePost = (id) => {
+        fetch(`http://localhost:8088/posts/${id}`, { method: 'DELETE' })
+            .then(() => {
+                fetch(`http://localhost:8088/posts?_expand=user`)
+            
+            .then(res => res.json())
+            .then((data) => {
+                updatePosts(data)
+            })
+            .then(() => {
+                history.push("/posts")
+            })})
+    
+
+    }
 
     return (
         <>
@@ -25,17 +40,19 @@ export const PostList = () => {
         posts.map(
             (completedPost) => {
                
-                return <center><div class="postList"><div key={`post--${completedPost.id}`}>
-                <img class="postImage" src={completedPost.imageUrl} /><p>
-                </p><div class="postData">
+                return <center><div className="postList"><div key={`post--${completedPost.id}`}>
+                <img className="postImage" src={completedPost.imageUrl} /><p>
+                </p><div className="postData">
                     <p>Title: {completedPost.title}</p>
                     <p>Description: {completedPost.description}</p>
                     <p>Posted by: {completedPost.user?.name}</p>
+                    <button onClick={() => deletePost(completedPost.id)}>Delete</button>
                 </div>
                 
                 </div>
                 </div>
                 </center>
+                
             }
         )
         }
