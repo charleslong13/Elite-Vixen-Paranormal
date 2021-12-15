d6668be123f20beef9a13adf21522a150767e1bc
charleslong13 <charleslong13@icloud.com>
2021-12-15 12:05:27 -0600

    csl-fadeCSS

--- src/components/contact/ContactUs.js
+++ src/components/contact/ContactUs.js
@@ -1,16 +1,16 @@
-
+import "./ContactUs.css"
 
 export const ContactUs = () => {
     return (
     <>
         <center><div className="contactUs">
-        <h3 className="contactUsTitle">
+        <h3 className="contactUs">
                     <i>You don't have to be from another realm to contact us!</i>
                 </h3>
-                <div className="contactUsBody">
+                <div className="contactUs">
                 <p><a href="https://www.facebook.com/evphunters/?ref=page_internal"><i>Elite Vixen Paranormal Facebook</i></a></p>
                 <a href="https://www.youtube.com/channel/UC0_XjDupXMcyVgZMzgWB9mQ"><i>Elite Vixen Paranormal Youtube</i></a>
-                <div className="contactInfo">
+                <div className="contactUs">
                 <p><i>Email: elitevixenparanormal@gmail.com</i></p>
                 <i>Phone: (615) 669-1474</i>
                 </div>
